60a1806456124e068b60fadbe6967941890b5d9b
jazmatician <jazmatician@gmail.com>
2015-04-15 17:19:37 -0400

    removed config entirely

--- TCSS/config1.js
+++
@@ -1,7 +0,0 @@
-﻿module.exports = {
-    consumer_key: '..'
-    , consumer_secret: '..'
-    , access_token: '3170462283-hxYSRgj96rgdvvkIgAo13pDpVLoTj1xR5L5m83C'
-    , access_token_secret: 'L3lWiKwYY0AZt3iGui7ibb7cpxY99DNTlwufFecl5Wbt3'
-    , delay: '100000'
-}
